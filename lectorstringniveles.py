cf5a6a8945ea4e41bf9a01a968d08501c396b114
exepa@hotmail.com <exepa@hotmail.com>
2023-07-23 14:11:35 -0300

    Sistema de Guardado funcional en lectordestring

--- lectorstringniveles.py
+++ lectorstringniveles.py
@@ -1,18 +1,59 @@
-texto = ''
-completo_texto = []
-while texto != 'Q':
 
-    texto = input('ingrese valores de string nivel:     ')
+def select_file():
+    file = input('Nombre de archivo a almacenar información:    ')
+    return file
 
-    texto = texto.replace('\t','')
+def grabar_archivo(f,lineas):
+    file = open(f, "r")
+    old_data = file.readlines()
+    new_data = old_data + lineas
 
-    texto = texto.replace('>', '')
-    if texto != 'Q':
-        completo_texto.append(texto)
+    file.close()
+    file = open(f, "w")
+    file.writelines(new_data)
+    file.write('\n')
+    file.close
 
 
+def input_text():
+    text = input('ingrese valores de string nivel:     ')
+    return text
 
-cadena = ''
-for line in completo_texto:
-    cadena = cadena + " " +  line
-print(cadena)
+def tab_replace(text):
+    text = str(text).replace('\t','')
+    return text
+
+def symbol_replace(text):
+    text = str(text).replace('>', '')
+    return text
+
+
+def read_text():
+    completo_texto = []
+    texto = ''
+    while texto != 'Q':
+
+        texto = input_text()
+
+        texto = tab_replace(texto)
+        print(texto)
+        texto = symbol_replace(texto)
+        if texto != 'Q':
+            texto = texto + " "
+            completo_texto.append(texto)
+
+    return completo_texto
+
+
+def print_cadena(completo_texto):
+    cadena = ''
+    for line in completo_texto:
+        cadena = cadena + " " +  line
+    print(cadena)
+    return completo_texto
+
+
+while True:
+    file = select_file()
+    lineas = print_cadena(read_text())
+    grabar_archivo(file,lineas)
